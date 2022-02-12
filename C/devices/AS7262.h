e80a5b0d7454980c8afd29cc2232bd689b70b865
lafalexis <ap.laframboise@gmail.com>
2022-02-12 14:45:55 -0500

    basic project structure, device header file and datasheet added

---
+++ C/devices/AS7262.h
@@ -0,0 +1,82 @@
+
+/*
+  AS7262 I2C device information and physical registers
+*/
+#define AS7262_SLAVE_ADDRESS_READ 0x93
+#define AS7262_SLAVE_ADDRESS_WRITE 0x92
+
+#define AS7262_STATUS_REGISTER 0x00
+#define AS7262_WRITE_REGISTER 0x01
+#define AS7262_READ_REGISTER 0x02
+
+/*
+  AS7262 I2C virtual register addresses
+*/
+// Version registers
+#define AS7262_HW_VERSION_L 0x00
+#define AS7262_HW_VERSION_H 0x01
+#define AS7262_FW_VERSION_L 0x02
+#define AS7262_FW_VERSION_H 0x03
+
+// Control registers
+#define AS7262_CR_CONTROL_SETUP 0x04
+#define AS7262_CR_INTEGRATION_TIME 0x05
+#define AS7262_CR_DEVICE_TEMPERATURE 0x06
+#define AS7262_CR_LED_CONTROL 0x07
+
+// Sensor raw data register
+#define AS7262_V_RAW_H 0x08
+#define AS7262_V_RAW_L 0x09
+#define AS7262_B_RAW_H 0x0A
+#define AS7262_B_RAW_L 0x0B
+#define AS7262_G_RAW_H 0x0C
+#define AS7262_G_RAW_L 0x0D
+#define AS7262_Y_RAW_H 0x0E
+#define AS7262_Y_RAW_L 0x0F
+#define AS7262_O_RAW_H 0x10
+#define AS7262_O_RAW_L 0x11
+#define AS7262_R_RAW_H 0x12
+#define AS7262_R_RAW_L 0x13
+
+// Sensor calibrated data register
+#define AS7262_V_CAL_0 0x14
+#define AS7262_V_CAL_1 0x15
+#define AS7262_V_CAL_2 0x16
+#define AS7262_V_CAL_3 0x17
+#define AS7262_B_CAL_0 0x18
+#define AS7262_B_CAL_1 0x19
+#define AS7262_B_CAL_2 0x1A
+#define AS7262_B_CAL_3 0x1B
+#define AS7262_G_CAL_0 0x1C
+#define AS7262_G_CAL_1 0x1D
+#define AS7262_G_CAL_2 0x1E
+#define AS7262_G_CAL_3 0x1F
+#define AS7262_Y_CAL_0 0x20
+#define AS7262_Y_CAL_1 0x21
+#define AS7262_Y_CAL_2 0x22
+#define AS7262_Y_CAL_3 0x23
+#define AS7262_O_CAL_0 0x24
+#define AS7262_O_CAL_1 0x25
+#define AS7262_O_CAL_2 0x26
+#define AS7262_O_CAL_3 0x27
+#define AS7262_R_CAL_0 0x28
+#define AS7262_R_CAL_1 0x29
+#define AS7262_R_CAL_2 0x2A
+#define AS7262_R_CAL_3 0x2B
+
+
+/*
+    Types definitions
+*/
+
+typedef struct AS7262_module{
+  
+} AS7262_module;
+
+typedef struct AS7262_config{
+
+} AS7262_config;
+
+/*
+    Functions prototypes
+*/

dfcb4b26bd16d7353f798da157a30f0059bd41f3
lafalexis <ap.laframboise@gmail.com>
2022-03-04 10:19:12 -0500

    AS7262 and HDC1080 integrated and functional (with 3rd party libraries). Saves data on sd card.

--- C/src/main/main.ino
+++ C/src/main/main.ino
@@ -1,9 +1,19 @@
-#include <DFRobot_DHT20.h>
 #include <SPI.h>
 #include <SD.h>
+#include <Wire.h>
 
-#define SAMPLING_S  1
-#define SAMPLING_MS 1000 * SAMPLING_S
+#include "Adafruit_AS726x.h"
+#include "ClosedCube_HDC1080.h"
+
+#define SAMPLING_S  (1)
+#define SAMPLING_MS (1000 * (SAMPLING_S - 0.5))
+
+#define NO_ERROR          (0)
+#define SD_ERROR          (1 << 0)
+#define AS7262_ERROR      (1 << 1)
+#define HDC1080_ERROR     (1 << 2)
+#define RTD_ERROR         (1 << 3)
+#define ANEMOMETER_ERROR  (1 << 4)
 
 struct Sensor_t;
 
@@ -19,18 +29,23 @@ union data_float_bytes {
   uint8_t bytes[sizeof(float)];
 };
 
-bool as7262_init(Sensor_t* sens);
-uint8_t as7262_read(Sensor_t* sens, uint8_t* data);
+void init_setup(void);
+int as7262_init(Sensor_t* sens);
+int hdc1080_init(Sensor_t* sens);
+int dht20_init(Sensor_t* sens);
 
-bool dht20_init(Sensor_t* sens);
+uint8_t as7262_read(Sensor_t* sens, uint8_t* data);
+uint8_t hdc1080_read(Sensor_t* sens, uint8_t* data);
 uint8_t dht20_read(Sensor_t* sens, uint8_t* data);
 
-void init_setup(void);
-
 void save_frame(uint8_t* data, uint8_t len);
+void signal_error(int err);
 
 Sensor_t as7262;
-Sensor_t dht20;
+Sensor_t hdc1080;
+
+Adafruit_AS726x as7262_sensor;
+ClosedCube_HDC1080 hdc1080_sensor;
 
 const int chipSelect = 10;
 
@@ -39,71 +54,60 @@ int main(){
   init();
 
   // Initializing peripherals and components
-  init_setup();
+  int err = 0;
+  if((err = init_setup()) != NO_ERROR){
+    signal_error(err);
+  }
 
   // Buffer to be saved on SD
-  uint8_t data[32] = {0};
+  uint8_t data[64] = {0};
   uint8_t ix = 0;
+  uint64_t datetime = 0;
 
   // Program loop
   while(true){
-    Serial.println("Printing indexes: ");
+    data[ix++] = (datetime & 0xFF00000000000000) >> 56;
+    data[ix++] = (datetime & 0x00FF000000000000) >> 48;
+    data[ix++] = (datetime & 0x0000FF0000000000) >> 40;
+    data[ix++] = (datetime & 0x000000FF00000000) >> 32;
+    data[ix++] = (datetime & 0x00000000FF000000) >> 24;
+    data[ix++] = (datetime & 0x0000000000FF0000) >> 16;
+    data[ix++] = (datetime & 0x000000000000FF00) >> 8;
+    data[ix++] = (datetime & 0x00000000000000FF);
     ix += as7262.sread(&as7262, data + ix);
-    Serial.println(ix);
-    ix += dht20.sread(&dht20, data + ix);
-    Serial.println(ix);
-    Serial.println("Printing data[32]: ");
-    for(int i = 0; i < 32; i++){
-      Serial.print(data[i]);
-      Serial.print("\t");
-    }
-    Serial.println("");
-  
+    ix += hdc1080.sread(&hdc1080, data + ix);
+
     save_frame(data, ix);
-    
-    ix = 0;
+
     delay(SAMPLING_MS);
+    datetime += SAMPLING_MS;
+    ix = 0;
   }
   
   return 0;
 }
 
-void init_setup(void){
+int init_setup(void){
+  int err = 0;
   Serial.begin(9600);
-
-  Serial.print("Initializing SD card...");
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
-  }
-  Serial.println("card initialized.");
-  
-  as7262_init(&as7262);
-  dht20_init(&dht20);
-
-  delay(500);
+  err |= sd_init();
+  err |= as7262_init(&as7262);
+  err |= hdc1080_init(&hdc1080);
+  return err;
 }
-
-/*
-void sd_init(){
-  
-}
-*/
 
 void save_frame(uint8_t* data, uint8_t len){
 
-  for(int i = 0; i < len; i++){
-    Serial.print(i);
-    Serial.print(" : ");
-    Serial.println(data[i]);
-  }
     // open the file. note that only one file can be open at a time,
     // so you have to close this one before opening another.
     File dataFile = SD.open("datalog.bin", FILE_WRITE);
-  
+    Serial.print("Frame lenght : "); Serial.println(len);
+    for(int i = 0; i < len; i++){
+      Serial.print(data[i]); 
+      Serial.print(",\t");
+    }
+    Serial.println();
+    
     // if the file is available, write to it:
     if (dataFile) {
       dataFile.write(data, len);
@@ -115,26 +119,46 @@ void save_frame(uint8_t* data, uint8_t len){
     }
 }
 
-bool as7262_init(Sensor_t* sens){
-  sens->sread = &as7262_read;
-}
-
-uint8_t as7262_read(Sensor_t* sens, uint8_t* data){
-
-  float data_float[6] = {13.7, 18.4, 12.6, 37.12, 53.9, 21.495};
-
-  data_float_bytes fb;
-
-  for(int i = 0; i < 6; i++){
-    fb.value = data_float[i];
-    for(int j = 0; j < sizeof(float); j++){
-      data[i * sizeof(float) + j] = fb.bytes[j];
-    }
+void signal_error(int err){
+  int errcnt = 0;
+  while(err > 0){
+    errcnt += err & 1;
+    err >>= 1;
   }
-  return 6 * sizeof(float);
 }
 
-bool dht20_init(Sensor_t* sens){
+int sd_init(){
+    // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    return 
+  }
+}
+
+int as7262_init(Sensor_t* sens){
+
+  //Adafruit_AS726x as7262_sensor;
+  if(!as7262_sensor.begin()){
+    Serial.println("could not connect to sensor! Please check your wiring.");
+    while(1);
+  }
+
+  sens->sensor_mod = (void*)&as7262_sensor;
+  
+  sens->sread = &as7262_read;
+  
+}
+
+int hdc1080_init(Sensor_t* sens){
+  
+  hdc1080_sensor.begin(0x40);
+  hdc1080_sensor.setResolution(HDC1080_RESOLUTION_11BIT, HDC1080_RESOLUTION_11BIT);
+
+  sens->sensor_mod = (void*)&hdc1080_sensor;
+  sens->sread = &hdc1080_read;
+}
+/*
+int dht20_init(Sensor_t* sens){
   DFRobot_DHT20 dht20_sensor;
   sens->sensor_mod = (void*)&dht20_sensor;
   
@@ -142,7 +166,56 @@ bool dht20_init(Sensor_t* sens){
 
   dht20_sensor.begin();
 }
+*/
+uint8_t as7262_read(Sensor_t* sens, uint8_t* data){
 
+  float measurements[AS726x_NUM_CHANNELS] = {0};
+  data_float_bytes fb;
+  
+  Adafruit_AS726x* pAs7262 = (Adafruit_AS726x*)sens->sensor_mod;
+  
+  pAs7262->startMeasurement(); //begin a measurement
+
+  while(!pAs7262->dataReady());
+  
+  pAs7262->readCalibratedValues(measurements);
+
+  for (int i = 0; i < AS726x_NUM_CHANNELS; i++){
+    fb.value = measurements[i];
+    /*
+    Serial.print("CH: "); Serial.print(i);
+    Serial.print("\t"); Serial.print(measurements[i]); Serial.print("\t");
+    */
+    for (int j = 0; j < sizeof(float); j++){
+      data[i * sizeof(float) + j] = fb.bytes[j];
+    }
+  }
+
+  //Serial.println();
+  
+  return 6 * sizeof(float);
+}
+
+uint8_t hdc1080_read(Sensor_t* sens, uint8_t* data){
+  ClosedCube_HDC1080* pHdc1080 = (ClosedCube_HDC1080*)sens->sensor_mod;
+  data_float_bytes temp;
+  data_float_bytes rh;
+  
+  temp.value = (float)(pHdc1080->readTemperature());
+  rh.value = (float)(pHdc1080->readHumidity());
+
+  /*
+  Serial.print("Temp: "); Serial.print(temp.value);
+  Serial.print("\tRH: "); Serial.println(rh.value);
+  */
+  
+  for (int i = 0; i < sizeof(float); i++){
+    data[i] = temp.bytes[i];
+    data[i + sizeof(float)] = rh.bytes[i];
+  }
+  return 2 * sizeof(float);
+}
+/*
 uint8_t dht20_read(Sensor_t* sens, uint8_t* data){
   DFRobot_DHT20* dht20_sensor = (DFRobot_DHT20*)sens->sensor_mod;
   data_float_bytes temp;
@@ -153,66 +226,9 @@ uint8_t dht20_read(Sensor_t* sens, uint8_t* data){
   for(int i = 0; i < sizeof(float); i++){
     data[i] = temp.bytes[i];
     data[i + sizeof(float)] = rh.bytes[i];
-    /*
-    Serial.print(i);
-    Serial.print("\t");
-    Serial.print(data[i]);
-    Serial.print("\t");
-    Serial.println(data[i + sizeof(float)]);
-    */
+    
   }
-  /*
-  Serial.print("temperature : ");
-  Serial.print(temp.value);
-  Serial.print("\thumidity : ");
-  Serial.println(rh.value);
-*/
   return sizeof(float) * 2;
   
-}
-
-/*
-void setup() {
-  Serial.begin(9600);
-
-  Serial.print("Initializing SD card...");
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
-  }
-  Serial.println("card initialized.");
-  
-  as7262_init(&as7262);
-  dht20_init(&dht20);
-
-  delay(2000);
-}
-
-void loop() {
-
-  uint8_t data[32] = {0};
-  uint8_t ix = 0;
-
-  while(true){
-    Serial.println("Printing indexes: ");
-    ix += as7262.sread(&as7262, data + ix);
-    Serial.println(ix);
-    ix += dht20.sread(&dht20, data + ix);
-    Serial.println(ix);
-    Serial.println("Printing data[32]: ");
-    for(int i = 0; i < 32; i++){
-      Serial.print(data[i]);
-      Serial.print("\t");
-    }
-    Serial.println("");
-  
-    save_frame(data, ix);
-    
-    ix = 0;
-    delay(SAMPLING_MS);
-  }
 }
 */

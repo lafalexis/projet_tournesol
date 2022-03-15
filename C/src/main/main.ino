bcf7a0501e4659861d84e959fedf59ffe727a3ef
lafalexis <ap.laframboise@gmail.com>
2022-03-15 17:53:29 -0400

    patchy but seems to work. Will need some cleanup and alarm timing seems weird

--- C/src/main/main.ino
+++ C/src/main/main.ino
@@ -1,14 +1,18 @@
 #include <SPI.h>
 #include <SD.h>
 #include <Wire.h>
+#include <avr/sleep.h>
+#include <avr/interrupt.h>
 
 #include "Adafruit_AS726x.h"
 #include "ClosedCube_HDC1080.h"
+#include "DS3231.h"
 
-#define SAMPLING_S        (1)
+#define SAMPLING_S        (5)
 #define SAMPLING_MS       (1000 * (SAMPLING_S - 0.5))
 
 #define ERROR_LED         (2)
+#define DS3231_EXTINT_PIN (3)
 
 #define NO_ERROR          (0)
 #define SD_ERROR          (1 << 0)
@@ -20,12 +24,18 @@
 
 #define ERROR_BLINK_MS    (200)
 
-#define SERIAL_EN                (0)
-#define DEBUG_AS7262_SERIAL      (0 & SERIAL_EN)
-#define DEBUG_HDC1080_SERIAL     (0 & SERIAL_EN)
-#define DEBUG_SAVE_FRAME_SERIAL  (0 & SERIAL_EN)
+#define SERIAL_EN                 (1)
+#define DEBUG_AS7262_SERIAL       (0 & SERIAL_EN)
+#define DEBUG_HDC1080_SERIAL      (0 & SERIAL_EN)
+#define DEBUG_SAVE_FRAME_SERIAL   (1 & SERIAL_EN)
+//#define DEBUG_SIGNAL_ERROR_SERIAL (1 & SERIAL_EN)
 
-#define DEBUG_NO_SD              (0)
+#define DEBUG_NO_SD               (0)
+
+#define DS3231_ALARM1_SEC         (30)
+#define DS3231_ALARM1_MIN         (30)
+#define DS3231_ALARM1_HOUR        (12)
+#define UNIX_CURRENT_TIMESTAMP    (1647380530)
 
 struct Sensor_t;
 
@@ -51,6 +61,8 @@ int init_setup(void);
 void status_blinker_init(void);
 void status_blinker_disable(void);
 
+int ds3231_init(uint8_t set_current_time);
+
 int as7262_init(Sensor_t* sens);
 int hdc1080_init(Sensor_t* sens);
 int dht20_init(Sensor_t* sens);
@@ -61,6 +73,9 @@ uint8_t dht20_read(Sensor_t* sens, uint8_t* data);
 
 void save_frame(uint8_t* data, uint8_t len);
 void signal_error(int err);
+
+void goto_sleep(void);
+
 
 // TODO: check if simple checksum is sufficient, crc table takes up lots of space.
 // https://stackoverflow.com/questions/22432066/how-to-use-table-based-crc-16-code
@@ -73,10 +88,14 @@ Sensor_t hdc1080;
 Adafruit_AS726x as7262_sensor;
 ClosedCube_HDC1080 hdc1080_sensor;
 
+DS3231_config_t ds3231_config = DS3231_CONFIG_DEFAULT;
+DS3231_alm_config_t alm1_config;
+
 const int chipSelect = 10;
 
 volatile int ledState = 0;
 volatile int initStatus = 0;
+volatile uint8_t wake_flag = 0;
 
 int main(){
   // Necessary to use int main() instead of void setup() & void loop()
@@ -92,12 +111,12 @@ int main(){
   uint8_t data[64] = {0};
   uint8_t ix = 0;
   uint16_t crc = 0;
-  uint64_t datetime = 0;
   data_uint64_bytes dt;
 
   // Program loop
   while(true){
-    dt.value = datetime;
+    
+    dt.value = DS3231_get_datetime();
     
     for (int i = sizeof(uint64_t) - 1; i >= 0; i--){
       data[ix++] = dt.bytes[i];
@@ -112,10 +131,9 @@ int main(){
     data[ix++] = (uint8_t)(crc & 0x00FF);
 
     save_frame(data, ix);
-
-    delay(SAMPLING_MS);
-    datetime += SAMPLING_MS;
     ix = 0;
+    Serial.println("going to sleep");
+    goto_sleep();
   }
   
   return 0;
@@ -124,6 +142,8 @@ int main(){
 int init_setup(void){
   int err = 0;
   status_blinker_init();
+
+  ds3231_init(0);
   
 #if SERIAL_EN
   Serial.begin(9600);
@@ -153,16 +173,6 @@ void save_frame(uint8_t* data, uint8_t len){
 
 #if !DEBUG_NO_SD
     File dataFile = SD.open("datalog.bin", FILE_WRITE);
-    
-    if (!dataFile) {
-      
-#if SERIAL_EN
-      Serial.print("ERROR : "); Serial.print(__FUNCTION__); Serial.println(" : SD card unreachable.");
-#endif
-
-      signal_error(SD_ERROR);
-    }
-    
     dataFile.write(data, len);
     dataFile.close();
 #endif
@@ -207,6 +217,32 @@ void signal_error(int err){
   }
 }
 
+int ds3231_init(uint8_t set_current_time){
+  //alm1_config.mask = 0xF;   // Every sec
+  alm1_config.mask = 0xE;   // Sec match
+  //alm1_config.mask = 0xC;   // Min and sec match
+  //alm1_config.mask = 0x8;   // Hour, min and sec match
+  //alm1_config.mask = 0x0;   // See source file for details...
+
+  alm1_config.sec = DS3231_ALARM1_SEC;
+  alm1_config.min = DS3231_ALARM1_MIN;
+  alm1_config.hour = DS3231_ALARM1_HOUR;
+  ds3231_config.pAlarm_1 = &alm1_config;
+  ds3231_config.pAlarm_2 = NULL;
+  
+  Serial.print("alm1 mask : "); Serial.println(ds3231_config.pAlarm_1->mask); 
+  Serial.print("alm1 sec : "); Serial.println(ds3231_config.pAlarm_1->sec); 
+  Serial.print("alm1 min : "); Serial.println(ds3231_config.pAlarm_1->min); 
+  Serial.print("alm1 hours : "); Serial.println(ds3231_config.pAlarm_1->hour); 
+  DS3231_init(&ds3231_config);
+
+  pinMode(DS3231_EXTINT_PIN, INPUT_PULLUP);
+
+  if (set_current_time){
+    DS3231_set_datetime(UNIX_CURRENT_TIMESTAMP); 
+  }
+}
+
 void status_blinker_init(void){
 
   pinMode(ERROR_LED, OUTPUT);
@@ -246,7 +282,7 @@ int sd_init(){
 
     return SD_ERROR;
   }
-  return 0;
+  return NO_ERROR;
 }
 
 int as7262_init(Sensor_t* sens){
@@ -264,7 +300,7 @@ int as7262_init(Sensor_t* sens){
   
   sens->sread = &as7262_read;
 
-  return 0;
+  return NO_ERROR;
 }
 
 int hdc1080_init(Sensor_t* sens){
@@ -280,7 +316,7 @@ int hdc1080_init(Sensor_t* sens){
   sens->sensor_mod = (void*)&hdc1080_sensor;
   sens->sread = &hdc1080_read;
 
-  return 0;
+  return NO_ERROR;
 }
 
 uint8_t as7262_read(Sensor_t* sens, uint8_t* data){
@@ -345,6 +381,58 @@ uint16_t calculate_crc16(uint16_t crc, const uint8_t *c_ptr, size_t len){
   return crc;
 }
 */
+
+void goto_sleep(void){
+    /*
+     * Borrowed from : 
+     * https://github.com/RalphBacon/192-DS3231-Wake-Up-Arduino/blob/master/YouTube%20Sketch2%20Sleep/Arduino_Sleep_DS3231_Wakeup.cpp
+     */
+    // Disable the ADC (Analog to digital converter, pins A0 [14] to A5 [19])
+    static byte prevADCSRA = ADCSRA;
+    ADCSRA = 0;
+
+    /* Set the type of sleep mode we want. Can be one of (in order of power saving):
+     SLEEP_MODE_IDLE (Timer 0 will wake up every millisecond to keep millis running)
+     SLEEP_MODE_ADC
+     SLEEP_MODE_PWR_SAVE (TIMER 2 keeps running)
+     SLEEP_MODE_EXT_STANDBY
+     SLEEP_MODE_STANDBY (Oscillator keeps running, makes for faster wake-up)
+     SLEEP_MODE_PWR_DOWN (Deep sleep)
+     */
+    set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+    sleep_enable();
+
+    // Turn of Brown Out Detection (low voltage)
+    // Thanks to Nick Gammon for how to do this (temporarily) in software rather than
+    // permanently using an avrdude command line.
+    //
+    // Note: Microchip state: BODS and BODSE only available for picoPower devices ATmega48PA/88PA/168PA/328P
+    //
+    // BODS must be set to one and BODSE must be set to zero within four clock cycles. This sets
+    // the MCU Control Register (MCUCR)
+    MCUCR = bit(BODS) | bit(BODSE);
+
+    // The BODS bit is automatically cleared after three clock cycles so we better get on with it
+    MCUCR = bit(BODS);
+
+    // Ensure we can wake up again by first disabling interupts (temporarily) so
+    // the wakeISR does not run before we are asleep and then prevent interrupts,
+    // and then defining the ISR (Interrupt Service Routine) to run when poked awake
+    noInterrupts();
+    attachInterrupt(digitalPinToInterrupt(3), sleepISR, LOW);
+
+    // Allow interrupts now
+    interrupts();
+
+    Serial.flush();
+
+    // And enter sleep mode as set above
+    sleep_cpu();
+
+    // Re-enable ADC if it was previously running
+    ADCSRA = prevADCSRA;
+}
+
 ISR(TIMER1_COMPA_vect){
   if(!initStatus){
     if(ledState){
@@ -354,4 +442,11 @@ ISR(TIMER1_COMPA_vect){
     }
     digitalWrite(ERROR_LED, ledState);
   }
+}
+
+void sleepISR(){
+  wake_flag = 1;
+  sleep_disable();
+  detachInterrupt(digitalPinToInterrupt(DS3231_EXTINT_PIN));
+  Serial.println("Waking up");
 }
